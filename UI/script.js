826c266bd7f8d2a6987d6c1a6708d219ab494d53
Dmitry Gavrushko <gavrushko98@gmail.com>
2018-03-05 18:57:46 +0300

    add script

---
+++ UI/script.js
@@ -0,0 +1,424 @@
+var photoPosts = [
+    {
+        id: '1',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '2',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '3',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '4',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '5',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '6',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '7',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '8',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '9',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-19'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '10',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '11',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '12',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '13',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '14',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '15',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '16',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#like', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '17',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '18',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-21'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '19',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-22'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#likeforlike', '#followme'],
+        likes: ['unknown', 'unknown']
+    },
+    {
+        id: '20',
+        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        hashtags: ['#like', '#followme'],
+        likes: ['unknown', 'unknown']
+    }
+
+];
+
+let mod = (function () {
+
+    function compareByDate(a, b) {
+        return a.createdAt - b.createdAt;
+    };
+
+    let getPhotoPosts = function (skip, top, filterConfig) {
+
+        photoPosts.sort(compareByDate);
+        let posts = photoPosts;
+
+        if (typeof (skip) !== 'number' || typeof (top) !== 'number')
+            return [];
+
+
+        if (filterConfig) {
+            if (filterConfig.author && (typeof(filterConfig.author) !== 'string' || filterConfig.author.length === 0) ||
+                (filterConfig.createdAt && filterConfig.createdAt == 'Invalid Date') ||
+                (filterConfig.hashtags && filterConfig.hashtags.every(item => item[0] !== '#')))
+                return [];
+
+            if (filterConfig.author)
+                posts = posts.filter(item => item.author === filterConfig.author);
+
+
+            if (filterConfig.createdAt)
+                posts = posts.filter(posts => Date.parse(posts.createdAt) === Date.parse(filterConfig.createdAt));
+
+
+            if (filterConfig.hashtags) {
+                posts = posts.filter(function (post) {
+                    if (typeof(post.hashtags) === 'undefined') {
+                        return false;
+                    }
+                    return filterConfig.hashtags.every(function (item) {
+                        return post.hashtags.includes(item);
+                    })
+                })
+            }
+        }
+
+
+        posts = posts.slice(skip, skip + top);
+        return posts;
+    };
+
+    let getPhotoPost = function (id) {
+        return photoPosts[photoPosts.findIndex(post => post.id === id)];
+    };
+
+
+    let validatePhotoPost = function (post, change) {
+
+        if (((!change && post.id && photoPosts.findIndex(photoPost => photoPost.id === post.id) === -1 && typeof(post.id) === 'string') || (change && !post.id)) &&
+            ((!change && post.description && typeof(post.description) === 'string' && post.description.length > 0 && post.description.length < 200) || (change && (post.description === 'undefiend' || (post.description && typeof(post.description) === 'string' && post.description.length > 0 && post.description.length < 200)))) &&
+            ((!change && post.createdAt && post.createdAt != 'Invalid Date') || (change && !post.createdAt)) &&
+            ((!change && post.author && typeof(post.author) === 'string' && post.author.length !== 0) || (change && !post.author)) &&
+            ((!change && post.photoLink && typeof(post.photoLink) === 'string' && post.photoLink.length !== 0) || (change && (post.photoLink === 'undefiend' || (post.photoLink && typeof(post.photoLink) === 'string' && post.photoLink.length !== 0)))) &&
+            (!post.hashtags || (post.hashtags && post.hashtags.every(item => item[0] === '#'))) &&
+            (!post.likes || (post.likes && post.likes.every(item => typeof(item) === 'string')))
+        ) {
+            return true;
+        }
+
+        return false;
+    };
+
+    let addPhotoPost = function (post) {
+        if (validatePhotoPost(post)) {
+            photoPosts.push(post);
+            photoPosts.sort(compareByDate);
+            return true;
+        }
+        return false;
+    };
+
+    let editPhotoPost = function (id, post) {
+        let changedPost = getPhotoPost(id);
+        if (changedPost && validatePhotoPost(post, true)) {
+            if (post.description)
+                changedPost.description = post.description;
+            if (post.photoLink)
+                changedPost.photoLink = post.photoLink;
+            if (post.hashtags)
+                changedPost.hashtags = post.hashtags;
+
+            photoPosts[photoPosts.findIndex(post => post.id === id)] = changedPost;
+            return true;
+        }
+        return false;
+    };
+
+
+    let removePhotoPost = function (id) {
+        let index = photoPosts.findIndex(item => item.id === id);
+        if (index !== -1) {
+            photoPosts.splice(index, 1);
+            return true;
+        }
+        return false;
+    };
+
+    return {
+        getPhotoPosts,
+        getPhotoPost,
+        validatePhotoPost,
+        addPhotoPost,
+        editPhotoPost,
+        removePhotoPost
+    }
+
+
+
+
+}());
+
+
+var CorrectPost = {
+    id: '45',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-03-03'),
+    author: 'Иван',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#like', '#followme'],
+    likes: ['unknown', 'unknown']
+}
+var InCorrectID = {
+    id: '1',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-03-03'),
+    author: 'Иван',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#like', '#followme'],
+    likes: ['unknown', 'unknown']
+}
+var InCorrectDescr = {
+    id: '45',
+    description: '',
+    createdAt: new Date('2017-03-03'),
+    author: 'Иван',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#like', '#followme'],
+    likes: ['unknown', 'unknown']
+}
+var InCorrectData = {
+    id: '45',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-23-43'),
+    author: 'Иван',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#like', '#followme'],
+    likes: ['unknown', 'unknown']
+}
+var InCorrectLink = {
+    id: '45',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-03-03'),
+    author: 'Иван',
+    hashtags: ['#like', '#followme'],
+    likes: ['unknown', 'unknown']
+}
+var InCorrectHash = {
+    id: '45',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-03-03'),
+    author: 'Иван',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['like', 'followme'],
+    likes: ['unknown', 'unknown']
+}
+var InCorrectLikes = {
+    id: '45',
+    description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+    createdAt: new Date('2017-03-03'),
+    author: 'Иван',
+    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    hashtags: ['#like', '#followme'],
+    likes: ['unknown', 24234]
+}
+
+
+console.log('Array of photoposts: ');
+console.log(photoPosts);
+console.log('*********************Testing method getPhotoPost********************');
+console.log("mod.getPhotoPost('1')");
+console.log(mod.getPhotoPost('1'));
+console.log("mod.getPhotoPost('127')");
+console.log(mod.getPhotoPost('127'));
+
+
+
+console.log('*********************Testing methods addPhotoPost and validatePhotoPost********************');
+console.log('Correct:');
+console.log(mod.addPhotoPost(CorrectPost));
+console.log('incorrect id: mod.addPhotoPost(InCorrectID)');
+console.log(mod.addPhotoPost(InCorrectID));
+console.log('incorrect description: mod.addPhotoPost(InCorrectDesc)');
+console.log(mod.addPhotoPost(InCorrectDescr));
+console.log('incorrect Data: mod.addPhotoPost(InCorrectData)');
+console.log(mod.addPhotoPost(InCorrectData));
+console.log('incorrect Link: mod.addPhotoPost(InCorrectLink)');
+console.log(mod.addPhotoPost(InCorrectLink));
+console.log('incorrect Hash-tags: mod.addPhotoPost(InCorrectHash)');
+console.log(mod.addPhotoPost(InCorrectHash));
+console.log('incorrect Likes: mod.addPhotoPost(InCorrectID)');
+console.log(mod.addPhotoPost(InCorrectLikes));
+
+
+
+console.log('*********************Testing methods editPhotoPost********************');
+console.log(mod.editPhotoPost('1', {description: 'hi'}));
+console.log(mod.getPhotoPost('1'));
+console.log('Incorrect (attempt to change id):');
+console.log(mod.editPhotoPost('1', {id: '2'}));
+console.log('Incorrect data:');
+console.log(mod.editPhotoPost('4', { description:''}));
+console.log("Edit more than one field:");
+console.log(mod.editPhotoPost('4', {description: 'new', hashtags: ['#its_me']}));
+console.log(mod.getPhotoPost('4'));
+
+console.log('*********************Testing methods removePhotoPost********************');
+console.log('Correct');
+console.log(mod.removePhotoPost('45'));
+console.log('Incorrect ID');
+console.log(mod.removePhotoPost('4333'));
+
+
+
+console.log('*********************Testing methods getPhotoPosts********************');
+console.log('******************************');
+console.log(mod.getPhotoPosts(0,10));
+console.log('******************************');
+console.log(mod.getPhotoPosts(3,5));
+console.log('******************************');
+console.log(mod.getPhotoPosts(0,10,{author:'Иван'}));
+console.log('******************************');
+console.log(mod.getPhotoPosts(0,10,{ author:'Иван', hashtags:['#like']}));
+console.log('******************************');
+console.log(mod.getPhotoPosts(0,10,{author:'LLBBB'}));
+console.log('******************************');
