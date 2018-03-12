c950e0ca02a004b9199921ad7dbd40ce0576d390
Dmitry Gavrushko <gavrushko98@gmail.com>
2018-03-13 01:36:15 +0300

    add DOM

---
+++ UI/DOM.js
@@ -0,0 +1,105 @@
+let modul = (function () {
+    let user = null;
+
+
+    let showPhotoPosts = function (post) {
+        let photoTable = document.querySelector(".photos-table");
+        let isUser = (user === post.author);
+
+        let Cell = document.createElement("div");
+        Cell.className = "cell";
+        Cell.setAttribute('id',post.id);
+
+
+        let insideCellInform = document.createElement("div");
+        insideCellInform.className = "inside-cell-inform";
+        insideCellInform.innerHTML = post.author + "<br>" + formatDate(post.createdAt);
+
+        let icons = document.createElement("div");
+        icons.className = "icons";
+        let trash = document.createElement("img");
+        trash.setAttribute('src', "img/trash.png");
+        trash.innerHTML = "<br>";
+        let edit = document.createElement("img");
+        edit.setAttribute('src', "img/edit.png");
+
+        let image = document.createElement("div");
+        image.className = "image";
+        let mainPhoto = document.createElement("img");
+        mainPhoto.setAttribute('src', post.photoLink);
+
+        let like = document.createElement("div");
+        like.className = "like icons";
+        let iconOfLikes = document.createElement("img");
+        iconOfLikes.src = 'img/not_like.png';
+
+        let countLikes = document.createElement("div");
+        countLikes.className = "count-likes";
+        countLikes.innerHTML = post.likes.length;
+
+        let backUnderPhoto = document.createElement("div");
+        backUnderPhoto.className = "back-under-photo";
+        let insideCellDescr = document.createElement("div");
+        insideCellDescr.className = "inside-cell-description";
+        insideCellDescr.innerHTML = post.hashtags.join('') + "<br>" + post.description;
+
+
+        backUnderPhoto.appendChild(insideCellDescr);
+        like.appendChild(iconOfLikes);
+        image.appendChild(mainPhoto);
+        icons.appendChild(edit);
+        icons.appendChild(trash);
+        Cell.appendChild(insideCellInform);
+        Cell.appendChild(icons);
+        Cell.appendChild(image);
+        Cell.appendChild(like);
+        Cell.appendChild(countLikes);
+        Cell.appendChild(backUnderPhoto);
+        photoTable.appendChild(Cell);
+    };
+
+
+    function formatDate(date) {
+
+        var dd = date.getDate();
+        if (dd < 10) dd = '0' + dd;
+
+        var mm = date.getMonth() + 1;
+        if (mm < 10) mm = '0' + mm;
+
+        var yy = date.getFullYear() % 100;
+        if (yy < 10) yy = '0' + yy;
+
+        return dd + '.' + mm + '.' + yy;
+    }
+
+    return {
+        showPhotoPosts
+    }
+
+}());
+
+
+function display(skip, top, filterConfig) {
+    document.body.getElementsByClassName("photos-table")[0].innerHTML="";
+    let array = mod.getPhotoPosts(skip, top, filterConfig);
+    for (let i = 0; i < array.length; i++) {
+        modul.showPhotoPosts(array[i]);
+    }
+}
+
+function addPost(post,skip, top, filterConfig) {
+    mod.addPhotoPost(post);
+    document.body.getElementsByClassName("photos-table")[0].innerHTML="";
+    let array = mod.getPhotoPosts(skip, top, filterConfig);
+    for (let i = 0; i < array.length; i++) {
+        modul.showPhotoPosts(array[i]);
+    }
+}
+
+function removePost(id) {
+    let post = document.getElementById(id);
+    post.remove();
+    mod.removePhotoPost(id);
+}
+
--- UI/script.js
+++ UI/script.js
@@ -1,174 +1,184 @@
 var photoPosts = [
     {
         id: '1',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
+        description: 'Морозный Петербург с высоты птичьего полёта.',
+        createdAt: new Date('2018-03-13'),
+        author: 'novostispb',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/c41039ee640e3c99d5af6fe861585fa7/5B35662A/t51.2885-15/s1080x1080/e35/28753987_190312381577199_47125016826871808_n.jpg',
+        hashtags: ['#arcitecture', '#city','#buildings'],
         likes: ['unknown', 'unknown']
     },
     {
         id: '2',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
-        likes: ['unknown', 'unknown']
+        description: 'Москва стала самым популярным регионом России среди иностранцев',
+        createdAt: new Date('2018-03-12'),
+        author: 'i_love_msk',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/a4504d5d2ed8b1cbae14e3aa7990525c/5B37B5C6/t51.2885-15/e35/28434498_611800972489341_7036478275752820736_n.jpg',
+        hashtags: ['#moscowcity', '#моястолица'],
+        likes: ['unknown', 'unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown']
     },
     {
         id: '3',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
+        description: 'В начале апреля на территории Парка Победы заработает танцплощадка под открытым небом, на которой профессиональные танцоры будут проводить занятия для всех желающих.',
+        createdAt: new Date('2018-03-10'),
+        author: 'i_love_msk',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/fc5db7c16c3f3d30f62603f2ea5b5778/5B444502/t51.2885-15/e35/28433266_790657854451334_6607685259364401152_n.jpg',
+        hashtags: ['#msk', '#moscow','#russia'],
         likes: ['unknown', 'unknown']
     },
     {
         id: '4',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
-        likes: ['unknown', 'unknown']
+        description: 'Москва заняла 1 е место в рейтинге российских регионов по качеству жизни.',
+        createdAt: new Date('2018-03-07'),
+        author: 'i_love_msk',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/fe3394ab30605a6a9ba5dc82fe293cb5/5B2D2768/t51.2885-15/e35/28435079_151218782218501_8228867278519664640_n.jpg',
+        hashtags: ['#msk', '#moskva','#moskow'],
+        likes: ['unknown', 'unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown']
     },
     {
         id: '5',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
-        likes: ['unknown', 'unknown']
+        description: 'Lookin up',
+        createdAt: new Date('2018-03-12'),
+        author: 'snoopdogg',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/2ee7aa75818b6838d41daad3bd3db319/5B2F5389/t51.2885-15/e35/28432715_345740675935898_6393319902567989248_n.jpg',
+        hashtags: ['#kushy', '#runnynose'],
+        likes: ['unknown', 'unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown']
     },
     {
         id: '6',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
-        likes: ['unknown', 'unknown']
+        description: 'With Ilon',
+        createdAt: new Date('2018-03-11'),
+        author: 'snoopdogg',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/63c0aa60bf66ae2e19fa291e2e343265/5B314923/t51.2885-15/e35/29087592_228397564388037_2297689473363738624_n.jpg',
+        hashtags: ['#change', '#the','#future'],
+        likes: ['unknown', 'unknown','unknown','unknown','unknown','unknown','unknown','unknown']
     },
     {
         id: '7',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
-        likes: ['unknown', 'unknown']
+        description: 'Father’s first',
+        createdAt: new Date('2018-03-02'),
+        author: 'snoopdogg',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/b81f5228c3dcbc193e54c7b8322ddbfd/5B4304EA/t51.2885-15/e35/28151075_158019448232423_3715546920387608576_n.jpg',
+        hashtags: ['#Fatherfirst', '#snoopdogg'],
+        likes: ['unknown', 'unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown']
     },
     {
         id: '8',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#followme'],
+        description: 'EEEEEEEEEEEEeeeeeeeeeeeeeeeeee!',
+        createdAt: new Date('2018-03-04'),
+        author: 'snoopdogg',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/4895c44756bfcb9d68d294af11b68f2c/5B488986/t51.2885-15/e35/28751147_752083074997759_326382287174238208_n.jpg',
+        hashtags: ['#snoop'],
         likes: ['unknown', 'unknown']
     },
     {
         id: '9',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-19'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
-        likes: ['unknown', 'unknown']
+        description: 'Photo by @kutovakika. A tiny forest springs out of Veronika’s (@kutovakika) hand. “Sometimes it’s the small landscapes we see every day that are the most beloved,” she writes. ',
+        createdAt: new Date('2018-03-06'),
+        author: 'instagram',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/d01303326627bb38101f231ae895dd37/5B327218/t51.2885-15/e35/28156491_297632824101208_7765735861160771584_n.jpg',
+        hashtags: ['#WHPlandscape'],
+        likes: []
     },
     {
         id: '10',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
-        likes: ['unknown', 'unknown']
+        description: 'Featured photo by @zrodyr\n' +
+        'Get outside and explore for our Weekend Hashtag Project: #WHPlandscape.\n' +
+        'Inject energy into an otherwise quiet moment. Capture a friend leaping in the air, or get low to include a patch of flowers in the foreground.',
+        createdAt: new Date('2018-03-03'),
+        author: 'instagram',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/4ccec7ee5c8732cf34032e50c59dae9a/5B325C77/t51.2885-15/e35/28151737_154910251887212_5250968772038098944_n.jpg',
+        hashtags: ['#WHPlandscape'],
+        likes: ['unknown', 'unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown']
     },
     {
         id: '11',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        description: 'Featured photo by @mambo926\n' +
+        'Weekend Hashtag Project: #WHPtwinning\n' +
+        'Ever looked up to realize you’re coincidentally wearing the same outfit as your BFF or co-worker? This weekend, the goal is to capture moments of shared likeness that make you smile.',
+        createdAt: new Date('2018-02-24'),
+        author: 'instagram',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/ac44937ff67194d708dd752691272864/5B44725C/t51.2885-15/e35/27892932_583607872031361_2532875212408487936_n.jpg',
         hashtags: ['#likeforlike', '#followme'],
         likes: ['unknown', 'unknown']
     },
     {
         id: '12',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        description: 'Photo by @tessledeux\n' +
+        'Sixteen-year-old Tess Ledeux (@tessledeux) still remembers her first backcountry ski experience in her hometown in the French Alps.',
         createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        author: 'instagram',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/ec966471c80dcc0908067f82f02b0749/5B2A78B7/t51.2885-15/e35/28157684_565061453855993_4607174365736337408_n.jpg',
         hashtags: ['#likeforlike', '#followme'],
         likes: ['unknown', 'unknown']
     },
     {
         id: '13',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
-        likes: ['unknown', 'unknown']
+        description: 'Photo by @simona_br_photography\n' +
+        'Looks like we’ve found the end of the 🌈. This welcome surprise pierced through the sky for #WHPlandscape.\n' +
+        'Follow along as we feature some of our favorite submissions to last weekend’s hashtag project.',
+        createdAt: new Date('2018-03-06'),
+        author: 'instagram',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/df313fdb6a9df246b1dd6995c8487aea/5B298852/t51.2885-15/e35/28434547_1113614052108663_1973801956598087680_n.jpg',
+        hashtags: ['#WHPlandscape'],
+        likes: ['unknown', 'unknown','unknown','unknown','unknown']
     },
     {
         id: '14',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
+        description: 'Photo by @smilewithjeryl\n' +
+        'A happy coincidence of #WHPtwinning in a Tokyo intersection.\n' +
+        'Follow along to see more of our favorites from last weekend’s hashtag project.',
+        createdAt: new Date('2018-02-27'),
+        author: 'instagram',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/a23e7d7a2f5506ebcdde27b52033a89f/5B30F49E/t51.2885-15/e35/27894188_150088839019069_1461789297400086528_n.jpg',
+        hashtags: ['#WHPtwinning'],
         likes: ['unknown', 'unknown']
     },
     {
         id: '15',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
-        likes: ['unknown', 'unknown']
+        description: 'great feed!',
+        createdAt: new Date('2018-03-15'),
+        author: 'exymore',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/6b0aad647d537d6bb5a7f59a53a41466/5B45E196/t51.2885-15/e35/23279797_1658279954223851_8744033156677500928_n.jpg',
+        hashtags: ['#vscorussia', '#bestvscobelarus', '#instagramrussia', '#instasng'],
+        likes: ['unknown', 'unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown','unknown']
     },
     {
         id: '16',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        description: 'Самое концептуальное фото',
+        createdAt: new Date('2018-03-02'),
+        author: 'exymore',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/32b9529ac0a442da6c7857d05bdd03bf/5B467532/t51.2885-15/e35/25038638_948632385288149_6085445857832861696_n.jpg',
         hashtags: ['#like', '#followme'],
         likes: ['unknown', 'unknown']
     },
     {
         id: '17',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
+        description: '',
+        createdAt: new Date('2018-04-07'),
+        author: 'exymore',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/a9aca956f9392f11f3463f10bd3c5ee7/5B2DAD26/t51.2885-15/e35/28433235_2012800552266665_5152045191196049408_n.jpg',
+        hashtags: ['#vscorussia', '#bestvscobelarus', '#instagramrussia', '#instasng'],
         likes: ['unknown', 'unknown']
     },
     {
         id: '18',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-21'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+        description: 'Смотри: впереди над тобою смеясь\n' +
+        'гордо годы твои идут!',
+        createdAt: new Date('2018-02-28'),
+        author: 'exymore',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/ac86b6e083837dd467b46b88c01ba40c/5B4C491A/t51.2885-15/e35/23099003_1917895798531610_2104949848133337088_n.jpg',
         hashtags: ['#likeforlike', '#followme'],
         likes: ['unknown', 'unknown']
     },
     {
         id: '19',
-        description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        description: '',
         createdAt: new Date('2018-02-22'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashtags: ['#likeforlike', '#followme'],
-        likes: ['unknown', 'unknown']
+        author: 'exymore',
+        photoLink: 'https://instagram.fbud2-1.fna.fbcdn.net/vp/493fe73e5930865f6dda62f9aff6e686/5B42B6FB/t51.2885-15/e35/21690614_1262363057196785_6024307835892924416_n.jpg',
+        hashtags: ['#minsk', '#belarus'],
+        likes: ['unknown', 'unknown','unknown,','unknown','unknown','unknown','unknown','unknown']
     },
     {
         id: '20',
@@ -188,7 +198,7 @@ let mod = (function () {
         return a.createdAt - b.createdAt;
     };
 
-    let getPhotoPosts = function (skip, top, filterConfig) {
+    let getPhotoPosts = function (skip = 0, top = 10, filterConfig) {
 
         photoPosts.sort(compareByDate);
         let posts = photoPosts;
@@ -299,12 +309,12 @@ let mod = (function () {
 }());
 
 
-var CorrectPost = {
+    var CorrectPost = {
     id: '45',
     description: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-    createdAt: new Date('2017-03-03'),
+    createdAt: new Date('2017-01-01'),
     author: 'Иван',
-    photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
+    photoLink: 'img/pattern2.jpg',
     hashtags: ['#like', '#followme'],
     likes: ['unknown', 'unknown']
 }
@@ -362,7 +372,7 @@ var InCorrectLikes = {
     likes: ['unknown', 24234]
 }
 
-
+/*
 console.log('Array of photoposts: ');
 console.log(photoPosts);
 console.log('*********************Testing method getPhotoPost********************');
@@ -422,3 +432,4 @@ console.log(mod.getPhotoPosts(0,10,{ author:'Иван', hashtags:['#like']}));
 console.log('******************************');
 console.log(mod.getPhotoPosts(0,10,{author:'LLBBB'}));
 console.log('******************************');
+*/
